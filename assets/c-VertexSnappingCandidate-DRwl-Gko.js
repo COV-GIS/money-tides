46f61dd0e7d46ba7cae04ee12ccbfc0e22fc7fc7
COV-GIS <COV-GIS@users.noreply.github.com>
2025-12-04 00:49:37 +0000

    deploy: 971b2336f77ae8c993de382af78343b9e52fe2bd

--- assets/c-VertexSnappingCandidate-DRwl-Gko.js
+++
@@ -1,1 +0,0 @@
-import{e as r}from"./c-hitTestSelectUtils-BAUzIyCw.js";import{b as e}from"./c-SketchViewModel-CLNvoROI.js";import{n as o}from"./c-PointSnappingHint-CiMYrHKY.js";class m extends e{constructor(t){super({...t,constraint:new r(t.targetPoint)})}get hints(){return[new o(this.targetPoint,this.isDraped,this.domain)]}}export{m as r};
